18ab990f716772855ae4200339e0e6135d68ffb3
asyroficahyadi <schoolkingdom9@gmail.com>
2021-09-19 22:28:10 +0700

    Penambahan fungsi BMI menggunakan fastapi

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-appbmi = FastAPI()
+app = FastAPI()
 
-@appbmi.get("/")
-async def root():
-    return {
-        "Main function"
-    }
+@app.get("/")
+async def root(height: float, weight: float):
+    height /= 100
+    bmi = round(weight / (height * height), 2)
+    if bmi < 18.5:
+        return JSONResponse(
+            content={
+                "bmi": bmi,
+                "label": "Underweight"},
+            status_code=200)
+    elif bmi >= 18.5 and bmi <= 24.9:
+        return JSONResponse(
+            content={
+                "bmi": bmi,
+                "label": "Normal weight"},
+            status_code=200)
+    elif bmi >= 25.0:
+        return JSONResponse(
+            content={
+                "bmi": bmi,
+                "label": "Overweight"},
+            status_code=200)
