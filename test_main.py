5e2a492e9a7f9459ce600225613905dc270ba401
asyroficahyadi <schoolkingdom9@gmail.com>
2021-09-20 11:10:01 +0700

    add branch python
    Signed-off-by: asyroficahyadi <schoolkingdom9@gmail.com>

--- test_main.py
+++ test_main.py
@@ -16,7 +16,7 @@ def test_read_if_no_value_main():
         'msg': 'field required', 'type': 
         'value_error.missing'}]}
 
-def test_read_if_string_main():
+def test_read_if_string_input_main():
     response = client.get("/?height=test&weight=test", json={'detail': [{'loc': ['query', 'height'], 'msg': 'value is not a valid float', 'type': 'type_error.float'}, {'loc': ['query', 'weight'], 'msg': 'value is not a valid float', 'type': 'type_error.float'}]})
     assert response.status_code == 422
     assert response.json() == {
